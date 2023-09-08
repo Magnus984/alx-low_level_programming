471fdea158933724fa4f349765e3dea964fd7edd
Magnus <nharnharyhaw984@gmail.com>
2023-09-08 00:40:00 +0000

    Last changes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
-#include<stdio.h>
-
+#include<unistd.h>
+#include<string.h>
 /**
  * main - Entry point
  *
@@ -7,7 +7,10 @@
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	size_t length = strlen(str1);
+
+	write(2, str1, length);
 	return (1);
 }
 
