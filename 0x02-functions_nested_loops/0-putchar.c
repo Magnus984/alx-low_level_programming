30d8e181664f79390b3de96193a75042f51ffb5b
Magnus <nharnharyhaw984@gmail.com>
2023-09-12 23:37:51 +0000

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "_putchar.c"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char output[8] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
+
+	for (i = 0; i < 8; i++)
+		_putchar(output[i]);
+	return (0);
+}
+
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+C source files of functions and nested loops
