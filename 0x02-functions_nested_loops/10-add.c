b3216a7bd85dfc487b6c52fb36b62b92b9ea5fe7
Magnus <nharnharyhaw984@gmail.com>
2023-09-14 00:55:48 +0000

    last mandatory

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - function
+ * @n: input one
+ * @m: input two
+ *
+ * Return: Always 0
+ */
+int add(int n, int m)
+{
+	int result = n + m;
+
+	return result;
+}
+
