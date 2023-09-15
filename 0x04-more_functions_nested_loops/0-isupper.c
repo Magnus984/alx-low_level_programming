644dfa572c9c4f36590bc8543d7f5d0665fa9714
Magnus <nharnharyhaw984@gmail.com>
2023-09-15 20:09:42 +0000

    first and second task

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+/**
+ * main - function that checks uppercase character
+ * @c: input varaible for function
+ *
+ * Return: 1 if c is uppercase otherwise 0;
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+/**
+ * _isdigit - checks for a digit between 0 and 9
+ * @c: input variable
+ *
+ * Return: 1 if c is a digit otherwise 0
+ */
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+		return (1);
+	else
+		return (0);
+	return (0);
+}
