7f63284c01236764d53eb713ee42170d971b147f
Magnus984 <143129932+Magnus984@users.noreply.github.com>
2023-09-19 23:26:46 +0000

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,10 +12,10 @@ void print_rev(char *s)
 
 	while (count >= 0)
 	{
-			if (s[count] == '\0')
-				break;
-			count++;
-		}
+		if (s[count] == '\0')
+			break;
+		count++;
+	}
 
 	for (count--; count >= 0; count--)
 		_putchar(s[count]);
