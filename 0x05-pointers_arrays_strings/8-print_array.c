fa5265f12697321a275abe678542763f95345c68
Magnus984 <143129932+Magnus984@users.noreply.github.com>
2023-09-19 23:38:46 +0000

    Update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -7,4 +7,15 @@
  *
  * Return: no return value
  */
+void print_array(int *a, int n)
+{
+	int i = 0;
 
+	for (; i < n; i++)
+	{
+		printf("%d", *(a + i));
+		if (i != (n - 1))
+			printf(", ");
+	}
+	printf("\n");
+}
