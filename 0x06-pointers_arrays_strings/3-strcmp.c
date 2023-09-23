06ad8543a0e0bc9ac6475c37b3c439a20d0d9c59
Magnus <nharnharyhaw984@gmail.com>
2023-09-23 16:41:10 +0000

    added new files and updated header file

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: pointer parameter
+ * @s2: pointer parameter
+ *
+ * Return: integer values(1, 0, -1)
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+
+	while ( *(s1 + i) != '\0')
+		i++;
+	while (*(s2 + j) != '\0')
+		j++;
+
+	if (i == j)
+	{
+		int k;
+		for (k = 0; k < i; k++)
+			if (s1[k] == s2[k])
+				continue;
+		if (k >= i)
+			return (0);
+	}
+	else if (i < j)
+	{
+		return (-15);
+	}
+	else if (i > j)
+	{
+		return (15);
+	}
+	return (0);
+}
---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes string
+ * @s - pointer variable to string to be capitalized
+ *
+ * Return: pointer
+ */
+char *cap_string(char *s)
+{
+	int i, j;
+
+	char sep[13] = {',', ';', '.', '!', '?', '"', '(', ')', '{', '}', ' ', '\n', '\t'};
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		for(j = 0; j < 13; j++)
+			if (s[i] == sep[j] && s[i + 1] != '\0' && s[i + 1] >= 'a' && s[i + 1] <= 'z')
+			{
+				s[i + 1] = s[i + 1] - ' ';
+			}
+	}
+	return (s);
+}
