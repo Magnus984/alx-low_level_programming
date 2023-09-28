99f2dcb3fb3c27751f73de699fec3b47e3444319
Magnus <nharnharyhaw984@gmail.com>
2023-09-28 21:11:51 +0000

    task 6

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,5 +9,7 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int supportive_function(int number, int sqrt);
 int _sqrt_recursion(int n);
+int support_function(unsigned int n, unsigned int a);
+int is_prime_number(int n);
 
 #endif
