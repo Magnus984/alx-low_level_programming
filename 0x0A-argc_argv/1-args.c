47be03280082a1832e6a4c825d62ae1787e0a2e9
Magnus <nharnharyhaw984@gmail.com>
2023-09-30 21:36:43 +0000

    first push

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: number of arguments
+ * @argv: array of command line arguments
+ *
+ * Return: 0 on Success
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
