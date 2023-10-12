3aca6a099cfd5254f640c2f09015be970e665bd4
Magnus <nharnharyhaw984@gmail.com>
2023-10-12 04:18:04 +0000

    task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes in each element array
+ * @array: input array
+ * @size: size of array
+ * @action: pointer to function
+ *
+ * Return: no return value
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int j;
+
+	for (j = 0; j < size; j++)
+		action(array[j]);
+
+}
