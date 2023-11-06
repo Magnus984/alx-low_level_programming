5a6c71d187f3ff671c6cb7e055150718182aae67
Magnus <nharnharyhaw984@gmail.com>
2023-11-06 22:23:36 +0000

    dog.h

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,2 @@
+type definition and structures
+
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+/**
+ * struct dog - structure called dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner
+ *
+ * Description: an object can be declared
+ * of type struct dog and would inherit its
+ * elements
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
