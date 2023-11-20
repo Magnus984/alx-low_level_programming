5e6b9f7fa9446a29a98233d2efcc25bf33c0425d
Magnus <nharnharyhaw984@gmail.com>
2023-11-20 13:55:20 +0000

    Add: Readme, headerfile and 1 .c file

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of
+ * a listint_t list.
+ * @h: head node.
+ *
+ * Return: the number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	unsigned int node_num;
+	const listint_t *ptr;
+
+	node_num = 0;
+	if (h == NULL)
+		exit(EXIT_FAILURE);
+	ptr = h;
+	while (ptr != NULL)
+	{
+		printf("%d\n", ptr->n);
+		ptr = ptr->next;
+		node_num += 1;
+	}
+	return (node_num);
+}
+
