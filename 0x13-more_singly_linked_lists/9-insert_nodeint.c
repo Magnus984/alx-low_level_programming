12cf6bdda08050fac6ae0c489a406d2f5f08e124
Magnus <nharnharyhaw984@gmail.com>
2023-11-21 13:46:20 +0000

    Fix: incorrect code output

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -37,12 +37,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		ptr = ptr->next;
 		index++;
 	}
-	if (idx == 1)
-	{
-		ptr2->next = *head;
-		*head = ptr2;
-		return (ptr2);
-	}
 	ptr2->next = ptr->next;
 	ptr->next = ptr2;
 	return (ptr2);
