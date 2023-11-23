51351234b997713eb9782e2337922562be691c10
Magnus <nharnharyhaw984@gmail.com>
2023-11-23 18:14:40 +0000

    Fix: loop condtion

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,12 +8,13 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned int start_printing, i, bit;
+	unsigned int start_printing, bit;
+	int i;
 
 	start_printing = 0;
-	for (i = 31; 1 >= 0; i--)
+	for (i = 31; i >= 0; i--)
 	{
-		bit = (n >> 1) & 1;
+		bit = (n >> i) & 1;
 		if (bit == 1)
 			start_printing = 1;
 		if (start_printing)
