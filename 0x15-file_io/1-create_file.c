5a93f8cbf47c1bf78ae967d1a4907dc53b278e56
Magnus <nharnharyhaw984@gmail.com>
2023-11-27 20:40:29 +0000

    Add: 1-create_file.c. Update: main.h

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: name of file
+ * @content: content to be written to file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, count;
+	ssize_t bytes_written;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_RDWR | O_TRUNC | O_CREAT, 0600);
+	if (fd == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		count = 0;
+		while (*(text_content + count) != '\0')
+			count++;
+		bytes_written = write(fd, text_content, count);
+		if (bytes_written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	close (fd);
+	return (1);
+}
+
+	
