93013492ecd2d77b7fb40556cffeb2e6a9c84c48
Magnus <nharnharyhaw984@gmail.com>
2023-11-28 00:10:22 +0000

    Add: 3-cp.c. Update: main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,9 +4,12 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <stdio.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void cp(int argc, char *argv[]);
+void close_dp(int fd);
 
 #endif
