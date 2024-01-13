3a118ebcc3bfd40561c6b73290f2d25b298fcb71
Magnus <nharnharyhaw984@gmail.com>
2024-01-13 14:12:11 +0000

    Fix: Added return value

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -26,6 +26,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			insertPtr->next = *(h);
 			(*h)->prev = insertPtr;
 			*h = insertPtr;
+			return (insertPtr);
 		}
 		else if (idx > 1)
 		{
