079c14e51f0ed6ecca7ef8406b216fbaefa75b24
Magnus Tetteh <143129932+Magnus984@users.noreply.github.com>
2024-02-03 01:20:40 +0000

    Update 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -36,6 +36,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (ptr == NULL)
 		return (0);
 	ptr->key = strdup(key);
+	if (ptr->key == NULL)
+	{
+		free(ptr);
+		return (0);
+	}
 	ptr->value = copyValue;
 	ptr->next = ht->array[index];
 	ht->array[index] = ptr;
