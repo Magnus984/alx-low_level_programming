a63ebf728db6f92f40be405c2f68daf2997814e6
Magnus <nharnharyhaw984@gmail.com>
2024-05-08 23:45:25 +0000

    advanced tasks

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,6 +6,11 @@
 /*Function prototypes*/
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
 /*Support functions*/
 void print_array(int L, int R, int *array);
+int min(int a, int b);
+int binarySearch(int *array, int L, int R, int value);
 #endif
